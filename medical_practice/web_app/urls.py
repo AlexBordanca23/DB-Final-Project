9c1c3c49a809643916027e89c1da4ca764afbd1d
AlexBordanca23 <bordanca@gmail.com>
2023-03-27 12:56:31 -0400

    Created views/urls for all models and updated base.html with a hover dropdown for CRUD functions on all models.

--- medical_practice/web_app/urls.py
+++ medical_practice/web_app/urls.py
@@ -12,10 +12,60 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('create/',views.doctors_create_view, name = 'create'),
-    path('view/',views.doctors_list_view, name = 'list_view'),
-    path('view/<int:id>',views.doctors_detail_view, name = "detail_view"),
-    path('update/<int:id>/',views.doctors_update_view,name = 'update'),
-    path('delete/<int:id>/', views.doctors_delete_view, name='delete'),
-    path('view_choices/',views.doctors_view_choices, name ='view_choices'),
+    
+    # Doctors views
+   path('doctors/create/', views.doctors_create_view, name='doctors-create'),
+   path('doctors/', views.doctors_list_view, name='doctors-list'),
+   path('doctors/detail/', views.doctors_detail_view, name='doctors-detail'),
+   path('doctors/choices/', views.doctors_view_choices, name='doctors-view-choices'),
+   path('doctors/update/<int:id>/', views.doctors_update_view, name='doctors-update'),
+   path('doctors/delete/<int:id>/', views.doctors_delete_view, name='doctors-delete'),
+
+   # PatientContact views
+   path('patient-contact/create/', views.pc_create_view, name='pc-create'),
+   path('patient-contact/', views.pc_list_view, name='pc-list'),
+   path('patient-contact/detail/', views.pc_detail_view, name='pc-detail'),
+   path('patient-contact/choices/', views.pc_view_choices, name='pc-view-choices'),
+   path('patient-contact/update/<int:id>/', views.pc_update_view, name='pc-update'),
+   path('patient-contact/delete/<int:id>/', views.pc_delete_view, name='pc-delete'),
+
+   # PatientDoctors views
+   path('patient-doctors/create/', views.pd_create_view, name='pd-create'),
+   path('patient-doctors/', views.pd_list_view, name='pd-list'),
+   path('patient-doctors/detail/', views.pd_detail_view, name='pd-detail'),
+   path('patient-doctors/choices/', views.pd_view_choices, name='pd-view-choices'),
+   path('patient-doctors/update/<int:id>/', views.pd_update_view, name='pd-update'),
+   path('patient-doctors/delete/<int:id>/', views.pd_delete_view, name='pd-delete'),
+
+   # PatientFinance views
+   path('patient-finance/create/', views.pf_create_view, name='pf-create'),
+   path('patient-finance/', views.pf_list_view, name='pf-list'),
+   path('patient-finance/detail/', views.pf_detail_view, name='pf-detail'),
+   path('patient-finance/choices/', views.pf_view_choices, name='pf-view-choices'),
+   path('patient-finance/update/<int:id>/', views.pf_update_view, name='pf-update'),
+   path('patient-finance/delete/<int:id>/', views.pf_delete_view, name='pf-delete'),
+
+   # PatientHealth views
+   path('patient-health/create/', views.ph_create_view, name='ph-create'),
+   path('patient-health/', views.ph_list_view, name='ph-list'),
+   path('patient-health/detail/', views.ph_detail_view, name='ph-detail'),
+   path('patient-health/choices/', views.ph_view_choices, name='ph-view-choices'),
+   path('patient-health/update/<int:id>/', views.ph_update_view, name='ph-update'),
+   path('patient-health/delete/<int:id>/', views.ph_delete_view, name='ph-delete'),
+   
+   # PatientVitals views
+   path('patient-vitals/create/', views.pv_create_view, name='pv-create'),
+   path('patient-vitals/', views.pv_list_view, name='pv-list'),
+   path('patient-vitals/detail/', views.pv_detail_view, name='pv-detail'),
+   path('patient-vitals/choices/', views.pv_view_choices, name='pv-view-choices'),
+   path('patient-vitals/update/<int:id>/', views.pv_update_view, name='pv-update'),
+   path('patient-vitals/delete/<int:id>/', views.pv_delete_view, name='pv-delete'),
+
+   # PatientId views
+   path('patient-id/create/', views.pid_create_view, name='pid-create'),
+   path('patient-id/', views.pid_list_view, name='pid-list'),
+   path('patient-id/detail/', views.pid_detail_view, name='pid-detail'),
+   path('patient-id/choices/', views.pid_view_choices, name='pid-view-choices'),
+   path('patient-id/update/<int:id>/', views.pid_update_view, name='pid-update'),
+   path('patient-id/delete/<int:id>/', views.pid_delete_view, name = 'pid-delete')
 ]
--- medical_practice/web_app/views.py
+++ medical_practice/web_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from .forms import DoctorForm
-from .models import Doctors
+from .forms import DoctorForm,PatientContactForm, PatientDoctorsForm, PatientFinanceForm, PatientHealthForm, PatientVitalsForm, PatientIdForm
+from .models import Doctors, PatientContact, PatientDoctors,PatientFinance, PatientHealth, PatientVitals, PatientId
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 # Create your views here.
@@ -9,6 +9,8 @@ def home(request):
     
     context = {}
     return render(request, 'index.html', context)
+
+# Doctors views
 
 def doctors_create_view(request):
     # dictionary for initial data with
@@ -71,3 +73,387 @@ def doctors_delete_view(request, id):
         doctor.delete()
         return redirect(home)
     return render(request, 'delete.html', {'doctor': doctor})
+
+# PatientContact views
+
+def pc_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientContactForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientContactForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def pc_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = PatientContact.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def pc_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientContact, patient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def pc_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def pc_update_view(request, id):
+    instance = get_object_or_404(PatientContact, paient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = PatientContactForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def pc_delete_view(request, id):
+    patient_contact = get_object_or_404(PatientContact, patient_id=id)
+    if request.method == 'POST':
+        patient_contact.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_contact': patient_contact})
+
+#PatientDoctors views
+
+def pd_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientDoctorsForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientDoctorsForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def pd_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = PatientDoctors.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def pd_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientDoctors, patient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def pd_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def pd_update_view(request, id):
+    instance = get_object_or_404(PatientDoctors, patient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = DoctorForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def pd_delete_view(request, id):
+    patient_doctors = get_object_or_404(PatientDoctors, patient_id=id)
+    if request.method == 'POST':
+        patient_doctors.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_doctors': patient_doctors})
+
+# PatientFinance views
+
+def pf_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientFinanceForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientFinanceForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def pf_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = PatientFinance.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def pf_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientFinance, patient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def pf_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def pf_update_view(request, id):
+    instance = get_object_or_404(PatientFinance, patient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = PatientFinanceForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def pf_delete_view(request, id):
+    patient_finance = get_object_or_404(PatientFinance, patient_id=id)
+    if request.method == 'POST':
+        patient_finance.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_finance': patient_finance})
+
+#PatientHealth views
+
+def ph_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientHealthForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientHealthForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def ph_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = PatientHealth.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def ph_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientHealth, patient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def ph_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def ph_update_view(request, id):
+    instance = get_object_or_404(PatientHealth, patient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = PatientHealthForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def ph_delete_view(request, id):
+    patient_health = get_object_or_404(PatientHealth, patient_id=id)
+    if request.method == 'POST':
+        patient_health.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_health': patient_health})
+
+# PatientVitals views
+
+def pv_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientVitalsForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientVitalsForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def pv_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = PatientVitals.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def pv_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientVitals, dpatient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def pv_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def pv_update_view(request, id):
+    instance = get_object_or_404(PatientVitals, patient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = PatientVitalsForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def pv_delete_view(request, id):
+    patient_vitals = get_object_or_404(PatientVitals, patient_id=id)
+    if request.method == 'POST':
+        patient_vitals.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_vitals': patient_vitals})
+
+# PatientId views
+
+def pid_create_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    form = PatientIdForm(request.POST)
+    if form.is_valid():
+        form.save()
+        
+    form = PatientIdForm()     
+    context['form']= form
+    return render(request, "create_view.html", context)
+
+
+def pid_list_view(request):
+    # dictionary for initial data with
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["dataset"] = Doctors.objects.all()[:30]
+         
+    return render(request, "list_view.html", context)
+
+
+def pid_detail_view(request):
+    
+   if request.method == 'POST':
+        # get the ID from the form data
+        id = request.POST.get('id')
+        # look up the model instance by ID
+        instance = get_object_or_404(PatientId, patient_id=id)
+        return render(request, 'detail_view.html', {'data': instance})
+   else:
+        return render(request, 'detail_view.html')
+    
+def pid_view_choices(request):
+    
+    context = {}
+    return render(request,'view_choice.html',context)
+
+def pid_update_view(request, id):
+    instance = get_object_or_404(PatientId, patient_id=id)
+
+    if request.method == 'POST':
+        # update the model instance with the form data
+        instance.name = request.POST.get('name')
+        instance.save()
+        return redirect(home)
+    else:
+        # prefill the form with the existing data
+        form = PatientIdForm(instance=instance)
+        return render(request, 'update.html', {'form': form})
+    
+def pid_delete_view(request, id):
+    patient_id = get_object_or_404(PatientId, patient_id=id)
+    if request.method == 'POST':
+        patient_id.delete()
+        return redirect(home)
+    return render(request, 'delete.html', {'patient_id': patient_id})
